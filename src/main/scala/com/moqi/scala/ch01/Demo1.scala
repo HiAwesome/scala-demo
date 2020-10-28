fc3ab73397e6c6bada9de09f92da6303727b9020
moqi <moqimoqidea@gmail.com>
2020-10-28 12:02:48 +0800

    测试 Scala and Java

--- src/main/scala/com/moqi/scala/ch01/Demo1.scala
+++ src/main/scala/com/moqi/scala/ch01/Demo1.scala
@@ -9,16 +9,38 @@ object Demo1 {
 
   def main(args: Array[String]): Unit = {
 
+    capital()
+
+    scalaFactorial()
+
+    javaFactorial()
+  }
+
+  private def capital(): Unit = {
     var capital = Map("US" -> "Washington", "France" -> "Paris")
     capital += ("Japan" -> "Tokyo")
     println(capital("France"))
+  }
 
-
+  private def scalaFactorial(): Unit = {
     def factorial(x: BigInt): BigInt =
       if (x == 0) 1 else x * factorial(x - 1)
 
     println(factorial(30))
   }
+
+  private def javaFactorial(): Unit = {
+    import java.math.BigInteger
+
+    def factorial(x: BigInteger): BigInteger =
+      if (x == BigInteger.ZERO)
+        BigInteger.ONE
+      else
+        x.multiply(factorial(x.subtract(BigInteger.ONE)))
+
+    println(factorial(new BigInteger("30")))
+  }
+
 
 }
 
