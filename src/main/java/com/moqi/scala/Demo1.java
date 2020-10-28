9d352655c2ac9cbe4f540465bb49847540bb444e
moqi <moqimoqidea@gmail.com>
2020-10-28 11:13:04 +0800

    init

---
+++ src/main/java/com/moqi/scala/Demo1.java
@@ -0,0 +1,13 @@
+package com.moqi.scala;
+
+/**
+ * @author moqi On 10/28/20 11:09
+ */
+
+public class Demo1 {
+
+    public static void main(String[] args) {
+        System.out.println("Demo1.main");
+    }
+
+}
