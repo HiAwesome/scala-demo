9c21b1dbd1f6fff1030c27bd5ca2af14426c231d
moqi <moqimoqidea@gmail.com>
2020-10-29 17:15:45 +0800

    Update A01CheckSumAccumulator

--- src/main/scala/com/moqi/scala/ch04/A01CheckSumAccumulator.scala
+++ src/main/scala/com/moqi/scala/ch04/A01CheckSumAccumulator.scala
@@ -6,5 +6,14 @@ package com.moqi.scala.ch04
  * @author moqi On 10/29/20 16:56
  */
 class A01CheckSumAccumulator {
-  var sum = 0
+  private var sum = 0
+
+  def add(b: Byte) : Unit = {
+    sum += b
+  }
+
+  def checkSum(): Int = {
+    ~(sum & 0xFF) + 1
+  }
+
 }
