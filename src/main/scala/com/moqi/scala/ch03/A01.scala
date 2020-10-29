2db042d34a97ea221a3ebb544be0e3d7e067d235
moqi <moqimoqidea@gmail.com>
2020-10-29 11:28:24 +0800

    Update 用类型参数化数组

--- src/main/scala/com/moqi/scala/ch03/A01.scala
+++ src/main/scala/com/moqi/scala/ch03/A01.scala
@@ -13,6 +13,18 @@ object A01 {
     val big = new BigInteger("12345")
     println(s"big = ${big}")
 
+    greet1
+    greet2
+
+    println((1).+(2))
+
+    val numNames = Array("zero", "one", "two")
+    println(s"numNames = ${numNames.mkString("Array(", ", ", ")")}")
+    val numNames2 = Array.apply("zero", "one", "two")
+    println(s"numNames2 = ${numNames2.mkString("Array(", ", ", ")")}")
+  }
+
+  private def greet1 = {
     val greetStrings = new Array[String](3)
 
     greetStrings(0) = "Hello"
@@ -24,4 +36,15 @@ object A01 {
     }
   }
 
+  private def greet2 = {
+    val greetStrings = new Array[String](3)
+
+    greetStrings.update(0, "Hello")
+    greetStrings.update(1, ", ")
+    greetStrings.update(2, "world!\n")
+
+    for (i <- 0 to 2) {
+      print(greetStrings.apply(i))
+    }
+  }
 }
