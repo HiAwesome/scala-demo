bcfa49562e49a35f63383cf5b4383a3dc2a8798c
moqi <moqimoqidea@gmail.com>
2020-10-29 11:30:29 +0800

    Add 使用列表

---
+++ src/main/scala/com/moqi/scala/ch03/A02.scala
@@ -0,0 +1,17 @@
+package com.moqi.scala.ch03
+
+/**
+ * 使用列表
+ *
+ * @author moqi On 10/29/20 11:29
+ */
+object A02 {
+
+  def main(args: Array[String]): Unit = {
+
+    val oneTwoThree = List(1, 2, 3)
+    println(s"oneTwoThree = ${oneTwoThree}")
+
+  }
+
+}
