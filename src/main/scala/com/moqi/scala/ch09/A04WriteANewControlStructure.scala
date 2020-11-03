cda42ea1768af1c617035122bffd0ae037dce33c
moqi <moqimoqidea@gmail.com>
2020-11-03 19:23:10 +0800

    Complete 编写新的控制结构

--- src/main/scala/com/moqi/scala/ch09/A04WriteANewControlStructure.scala
+++ src/main/scala/com/moqi/scala/ch09/A04WriteANewControlStructure.scala
@@ -10,15 +10,34 @@ import java.util.Date
  */
 object A04WriteANewControlStructure {
 
+  private val filePath = "./src/main/resources/output.txt"
+
   def main(args: Array[String]): Unit = {
 
     println(s"twice(_ + 10, 5) = ${twice(_ + 10, 5)}")
     println()
 
     withPrintWriter(
-      new File("./src/main/resources/output.txt"),
+      new File(filePath),
       writer => writer.println(new Date)
     )
+    println("withPrintWriter over")
+
+    val file = new File(filePath)
+
+    withPrintWriter2(file) { writer => writer.println("Hello World !!!!!") }
+    println("withPrintWriter2 over")
+
+  }
+
+  def withPrintWriter2(file: File)(op: PrintWriter => Unit): Unit = {
+    val writer = new PrintWriter(file)
+
+    try {
+      op(writer)
+    } finally {
+      writer.close()
+    }
 
   }
 
