11c116634aa53ff643b2e4974c2c6af71518bd4f
moqi <moqimoqidea@gmail.com>
2020-11-03 17:27:15 +0800

    Finish 减少代码重复

--- src/main/scala/com/moqi/scala/ch09/A01ReduceCodeDuplication.scala
+++ src/main/scala/com/moqi/scala/ch09/A01ReduceCodeDuplication.scala
@@ -23,10 +23,37 @@ object A01ReduceCodeDuplication {
       println(s"file.getName = ${file.getName}")
     println()
 
+    for (file <- filesEnding2(".scala"))
+      println(s"file.getName = ${file.getName}")
+    println()
+
+    for (file <- filesContaining2("Reduce"))
+      println(s"file.getName = ${file.getName}")
+    println()
+
+    for (file <- filesRegex2(".*Reduce.*"))
+      println(s"file.getName = ${file.getName}")
+    println()
+
+    for (file <- filesEnding3(".scala"))
+      println(s"file.getName = ${file.getName}")
+    println()
+
+    for (file <- filesContaining3("Reduce"))
+      println(s"file.getName = ${file.getName}")
+    println()
+
+    for (file <- filesRegex3(".*Reduce.*"))
+      println(s"file.getName = ${file.getName}")
+    println()
+
   }
 
   def fileHere = new File("./src/main/scala/com/moqi/scala/ch09").listFiles()
 
+  /**
+   * 第一版本代码
+   */
   def filesEnding(query: String) =
     for (file <- fileHere; if file.getName.endsWith(query))
       yield file
@@ -38,5 +65,33 @@ object A01ReduceCodeDuplication {
   def filesRegex(query: String) =
     for (file <- fileHere; if file.getName.matches(query))
       yield file
+
+  /**
+   * 第二版本代码
+   */
+  def filesMatching2(query: String, matcher: (String, String) => Boolean) =
+    for (file <- fileHere; if matcher(file.getName, query))
+      yield file
+
+  def filesEnding2(query: String) = filesMatching2(query, _.endsWith(_))
+
+  def filesContaining2(query: String) = filesMatching2(query, _.contains(_))
+
+  def filesRegex2(query: String) = filesMatching2(query, _.matches(_))
+
+
+  /**
+   * 第三版本代码
+   */
+  def filesMatching3(matcher: String => Boolean) =
+    for (file <- fileHere; if matcher(file.getName))
+      yield file
+
+  def filesEnding3(query: String) = filesMatching3(_.endsWith(query))
+
+  def filesContaining3(query: String) = filesMatching3(_.contains(query))
+
+  def filesRegex3(query: String) = filesMatching3(_.matches(query))
+
 
 }
