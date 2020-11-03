b2836419a1e257e9797bb0e052459fa8366152f5
moqi <moqimoqidea@gmail.com>
2020-11-03 17:38:37 +0800

    简化调用方代码

---
+++ src/main/scala/com/moqi/scala/ch09/A02SimplifyCallerCode.scala
@@ -0,0 +1,58 @@
+package com.moqi.scala.ch09
+
+/**
+ * 简化调用方代码
+ *
+ * @author moqi On 11/3/20 17:30
+ */
+object A02SimplifyCallerCode {
+
+  def main(args: Array[String]): Unit = {
+
+    println(s"containsNeg(List(1, 2, 3, 4)) = ${containsNeg(List(1, 2, 3, 4))}")
+    println(s"containsNeg(List(1, 2, -3, 4)) = ${containsNeg(List(1, 2, -3, 4))}")
+    println()
+
+    println(s"containsNeg2(Nil) = ${containsNeg2(Nil)}")
+    println(s"containsNeg2(List(0, -1, -2)) = ${containsNeg2(List(0, -1, -2))}")
+    println()
+
+    println(s"containsOdd(List(1, 2, 3, 4)) = ${containsOdd(List(1, 2, 3, 4))}")
+    println(s"containsOdd(List(2, 4)) = ${containsOdd(List(2, 4))}")
+    println()
+
+    println(s"containsOdd2(Nil) = ${containsOdd2(Nil)}")
+    println(s"containsOdd2(List(0, -1)) = ${containsOdd2(List(0, -1))}")
+    println()
+
+  }
+
+  def containsOdd2(nums: List[Int]): Boolean = nums.exists(_ % 2 == 1)
+
+  def containsOdd(nums: List[Int]): Boolean = {
+    var exists = false
+
+    for (num <- nums)
+      if (num % 2 == 1) {
+        exists = true
+        return exists
+      }
+
+    exists
+  }
+
+  def containsNeg2(nums: List[Int]): Boolean = nums.exists(_ < 0)
+
+  def containsNeg(nums: List[Int]): Boolean = {
+    var exists = false
+
+    for (num <- nums)
+      if (num < 0) {
+        exists = true
+        return exists
+      }
+
+    exists
+  }
+
+}
