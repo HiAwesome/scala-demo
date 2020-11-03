6f98ebb869942e1383015a19d37414f9b0a52ccd
moqi <moqimoqidea@gmail.com>
2020-11-03 16:26:25 +0800

    Update 尾递归

--- src/main/scala/com/moqi/scala/ch08/A09TailRecursion.scala
+++ src/main/scala/com/moqi/scala/ch08/A09TailRecursion.scala
@@ -13,9 +13,27 @@ object A09TailRecursion {
 
     //boom(3)
 
-    bang(3)
+    //bang(3)
+
+    println(s"isOdd(10) = ${isOdd(10)}")
+    println()
+
+    nestedFun(3)
 
   }
+
+  /**
+   * 最后一步调用的是一个函数值，而不是发起调用的那个函数自己，也无法被优化为尾递归
+   */
+  val funValue = nestedFun _
+  def nestedFun(x: Int): Unit =
+    if (x != 0) {println(x); funValue(x - 1)}
+
+  /**
+   * 受限于 JVM 指令集，两个互相递归的函数无法优化为尾递归
+   */
+  def isEven(x: Int): Boolean = if (x == 0) true else isOdd(x - 1)
+  def isOdd(x: Int): Boolean = if (x == 0) false else isEven(x - 1)
 
   /**
    * 不是尾递归的函数，会出现多个栈帧
