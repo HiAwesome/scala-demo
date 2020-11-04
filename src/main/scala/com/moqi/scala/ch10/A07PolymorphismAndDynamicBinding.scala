137bb0334883aadc55232fe0851a82367c980c1f
moqi <moqimoqidea@gmail.com>
2020-11-04 18:02:44 +0800

    Update 多态和动态绑定

--- src/main/scala/com/moqi/scala/ch10/A07PolymorphismAndDynamicBinding.scala
+++ src/main/scala/com/moqi/scala/ch10/A07PolymorphismAndDynamicBinding.scala
@@ -7,6 +7,14 @@ package com.moqi.scala.ch10
  */
 object A07PolymorphismAndDynamicBinding {
 
+  def main(args: Array[String]): Unit = {
+
+    val e1: A07AbstractElement = new A07ArrayElement(Array("Hello", "world"))
+    val ae: A07ArrayElement = new A07LineElement("Hello")
+    val e2: A07AbstractElement = ae
+    val e3: A07AbstractElement = new A07UniformElement('x', 2, 3)
+
+  }
 
 }
 
@@ -20,7 +28,21 @@ abstract class A07AbstractElement {
 
 }
 
-class UniformElement(
+class A07ArrayElement(private val conts: Array[String]) extends A07AbstractElement {
+
+  override val contents: Array[String] = conts
+
+}
+
+class A07LineElement(s: String) extends A07ArrayElement(Array(s)) {
+
+  override def width: Int = s.length
+
+  override def height: Int = 1
+
+}
+
+class A07UniformElement(
                       ch: Char,
                       override val width: Int,
                       override val height: Int,
