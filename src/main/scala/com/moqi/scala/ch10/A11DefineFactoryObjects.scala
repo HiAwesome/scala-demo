3215aaa20e9008d1f055a5ea2bdcd241137d8d3e
moqi <moqimoqidea@gmail.com>
2020-11-05 10:08:37 +0800

    Update 定义工厂对象

--- src/main/scala/com/moqi/scala/ch10/A11DefineFactoryObjects.scala
+++ src/main/scala/com/moqi/scala/ch10/A11DefineFactoryObjects.scala
@@ -18,6 +18,34 @@ object A11AbstractElement {
   def elem(char: Char, width: Int, height: Int): A11AbstractElement =
     new A11UniformElement(char, width, height)
 
+  private class A11ArrayElement(private val conts: Array[String]) extends A11AbstractElement {
+
+    override val contents: Array[String] = conts
+
+  }
+
+  private class A11LineElement(s: String) extends A11AbstractElement {
+
+    val contents = Array(s)
+
+    override def width: Int = s.length
+
+    override def height: Int = 1
+
+  }
+
+  private class A11UniformElement(
+                                   ch: Char,
+                                   override val width: Int,
+                                   override val height: Int,
+                                 ) extends A11AbstractElement {
+
+    private val line = ch.toString * width
+
+    override def contents: Array[String] = Array.fill(height)(line)
+
+  }
+
 }
 
 abstract class A11AbstractElement {
@@ -32,13 +60,13 @@ abstract class A11AbstractElement {
    * 将两个元素拼接在一起
    */
   def above(that: A11AbstractElement): A11AbstractElement =
-    new A11ArrayElement(this.contents ++ that.contents)
+    A11AbstractElement.elem(this.contents ++ that.contents)
 
   /**
    * 将两个元素并排放在一起
    */
   def beside(that: A11AbstractElement): A11AbstractElement = {
-    new A11ArrayElement(
+    A11AbstractElement.elem(
       for (
         (line1, line2) <- this.contents zip that.contents
       ) yield line1 + line2
@@ -52,30 +80,3 @@ abstract class A11AbstractElement {
 
 }
 
-class A11ArrayElement(private val conts: Array[String]) extends A11AbstractElement {
-
-  override val contents: Array[String] = conts
-
-}
-
-class A11LineElement(s: String) extends A11AbstractElement {
-
-  val contents = Array(s)
-
-  override def width: Int = s.length
-
-  override def height: Int = 1
-
-}
-
-class A11UniformElement(
-                         ch: Char,
-                         override val width: Int,
-                         override val height: Int,
-                       ) extends A11AbstractElement {
-
-  private val line = ch.toString * width
-
-  override def contents: Array[String] = Array.fill(height)(line)
-
-}
