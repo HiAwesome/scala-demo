0aba39230da6a489119ad8ccbdbb579c5256ae85
moqi <moqimoqidea@gmail.com>
2020-11-06 17:30:26 +0800

    隐式导入

---
+++ src/main/scala/com/moqi/scala/ch13/A04ImplicitImport.scala
@@ -0,0 +1,14 @@
+package com.moqi.scala.ch13
+
+/**
+ * 隐式导入
+ *
+ * import java.lang._
+ * import scala._
+ * import Predef._
+ *
+ * @author moqi On 11/6/20 17:29
+ */
+object A04ImplicitImport {
+
+}
