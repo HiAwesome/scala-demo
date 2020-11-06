e66375dfa214cfe16da7b775655ac93c526b238f
moqi <moqimoqidea@gmail.com>
2020-11-06 15:17:44 +0800

    瘦借口和富借口

---
+++ src/main/scala/com/moqi/scala/ch12/A02ThinInterfacesAndRichExcuses.scala
@@ -0,0 +1,17 @@
+package com.moqi.scala.ch12
+
+/**
+ * 瘦借口和富借口
+ *
+ * @author moqi On 11/6/20 15:14
+ */
+object A02ThinInterfacesAndRichExcuses {
+
+}
+
+trait A02CharSequence {
+  def charAt(index: Int): Char
+  def length: Int
+  def subSequence(start: Int, end: Int): CharSequence
+  override def toString: String = String
+}
