a88f166d4b3a6a69b3bf1ad0ec474e705fcbc7ab
moqi <moqimoqidea@gmail.com>
2020-11-06 16:58:06 +0800

    关于 特质、多重继承、抽象类 的讨论

---
+++ src/main/scala/com/moqi/scala/ch12/A06WhyNotUseMultipleInheritance.scala
@@ -0,0 +1,11 @@
+package com.moqi.scala.ch12
+
+/**
+ * 为什么不用多重继承
+ * 特质中的 super 调用的方法取决于类和混入该类的特质的线性化 linearization
+ *
+ * @author moqi On 11/6/20 16:16
+ */
+object A06WhyNotUseMultipleInheritance {
+
+}
