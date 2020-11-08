dbbd68f8c23cbe215375de6ac90ca89482de525d
moqi <moqimoqidea@gmail.com>
2020-11-08 12:01:55 +0800

    基于性质的测试

---
+++ src/main/scala/com/moqi/scala/ch14/A05PropertyBasedTesting.scala
@@ -0,0 +1,37 @@
+package com.moqi.scala.ch14
+
+import com.moqi.scala.ch14.AbstractElement.elem
+import org.scalatest.matchers.must.Matchers._
+import org.scalatest.wordspec.AnyWordSpec
+import org.scalatestplus.scalacheck.ScalaCheckPropertyChecks
+
+/**
+ * 基于性质的测试
+ * 模拟书中示例代码 14.7 发生编译错误，forAll 无法编译通过
+ *
+ * Scala 编程第三版第十四节地址
+ *  https://booksites.artima.com/programming_in_scala_3ed/examples/html/ch14.html#sec5
+ * Scala 编程第四版第十四节地址
+ *  https://booksites.artima.com/programming_in_scala_4ed/examples/html/ch14.html#sec5
+ *
+ * 根据第四版的代码，引入 scalatestplus-scalacheck_2.13 依赖解决问题
+ *
+ * @author moqi On 11/8/20 09:51
+ */
+object A05PropertyBasedTesting {
+
+}
+
+class A05ElementSpec extends AnyWordSpec with ScalaCheckPropertyChecks {
+
+  "elem result" must {
+    "have passed width" in {
+      forAll { (w: Int) =>
+        whenever(w > 0) {
+          elem('x', w % 100, 3).width must equal(w % 100)
+        }
+      }
+    }
+  }
+
+}
