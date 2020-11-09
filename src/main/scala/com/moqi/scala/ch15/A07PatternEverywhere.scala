7102e6be3d654c7a2ab053f189b660156bf14c56
moqi <moqimoqidea@gmail.com>
2020-11-09 17:49:29 +0800

    Complete 到处都是模式

--- src/main/scala/com/moqi/scala/ch15/A07PatternEverywhere.scala
+++ src/main/scala/com/moqi/scala/ch15/A07PatternEverywhere.scala
@@ -1,5 +1,7 @@
 package com.moqi.scala.ch15
 
+import com.moqi.scala.ch13.bobs.Fruits.{Apple, Orange}
+
 /**
  * 到处都是模式
  *
@@ -11,8 +13,75 @@ object A07PatternEverywhere {
 
     func1
 
+    func2
+
+    func3
+
   }
 
+  /**
+   * for 表达式中的模式
+   */
+  private def func3: Unit = {
+    // 带有元祖模式的 for 表达式，匹配永远不会失败
+    for ((country, city) <- Map("France" -> "Paris", "Japan" -> "Tokyo"))
+      println("The capital of " + country + " is " + city)
+    println()
+    // 从列表中选取匹配特定模式的元素
+    val results = List(Some(Apple), None, Some(Orange))
+    for (Some(fruit) <- results)
+      println(s"fruit = ${fruit}")
+    println()
+  }
+
+  /**
+   * 作为偏函数的 case 序列
+   */
+  private def func2: Unit = {
+    val withDefault: Option[Int] => Int = {
+      case Some(x) => x
+      case None => 0
+    }
+    println(s"withDefault(Some(10)) = ${withDefault(Some(10))}")
+    println(s"withDefault(None) = ${withDefault(None)}")
+    println()
+
+    val second: List[Int] => Int = {
+      case x :: y :: _ => y
+    }
+    println(s"second(List(1, 2, 3)) = ${second(List(1, 2, 3))}")
+    try {
+      println(s"second(List()) = ${second(List())}")
+    } catch {
+      case e: MatchError => println(s"e.getMessage() = ${e.getMessage()}")
+    }
+    println()
+
+    val secondPartial: PartialFunction[List[Int], Int] = {
+      case x :: y :: _ => y
+    }
+    println(s"secondPartial.isDefinedAt(List(1, 2, 3)) = ${secondPartial.isDefinedAt(List(1, 2, 3))}")
+    println(s"secondPartial(List(1, 2, 3)) = ${secondPartial(List(1, 2, 3))}")
+    println(s"secondPartial.isDefinedAt(List()) = ${secondPartial.isDefinedAt(List())}")
+    try {
+      println(s"secondPartial(List()) = ${secondPartial(List())}")
+    } catch {
+      case e: MatchError => println(s"e.getMessage() = ${e.getMessage()}")
+    }
+    println()
+
+    // secondPartial 将被翻译成如下的偏函数
+    new PartialFunction[List[Int], Int] {
+      override def apply(xs: List[Int]): Int = xs match {
+        case x :: y :: _ => y
+      }
+
+      override def isDefinedAt(xs: List[Int]): Boolean = xs match {
+        case x :: y :: _ => true
+        case _ => false
+      }
+    }
+  }
 
   /**
    * 变量定义中的模式
