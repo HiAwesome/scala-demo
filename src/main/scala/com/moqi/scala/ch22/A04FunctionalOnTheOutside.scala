4874d3c2a25b07f3a2e52aefc66401423443c7fa
moqi <moqimoqidea@gmail.com>
2020-11-20 16:42:01 +0800

    外部可见的函数式

---
+++ src/main/scala/com/moqi/scala/ch22/A04FunctionalOnTheOutside.scala
@@ -0,0 +1,13 @@
+package com.moqi.scala.ch22
+
+/**
+ * 外部可见的函数式
+ *
+ * Scala 的 List 和 ListBuffer 的设计和 Java 的 String 和 StringBuffer 类很相似，这不是巧合。
+ * 在这两种情况下，设计者们都想在保持纯的不可变的数据结构的同时提供一种高效的、渐进式的构造方式。
+ *
+ * @author moqi On 11/20/20 16:38
+ */
+object A04FunctionalOnTheOutside {
+
+}
