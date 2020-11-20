5bae664247a4be49f5a7e17c5cb066211363c2f4
moqi <moqimoqidea@gmail.com>
2020-11-20 10:33:01 +0800

    隐式规则

--- src/main/scala/com/moqi/scala/ch21/A02RulesForImplicits.scala
+++ src/main/scala/com/moqi/scala/ch21/A02RulesForImplicits.scala
@@ -10,12 +10,19 @@ object A02RulesForImplicits {
   def main(args: Array[String]): Unit = {
 
 
-
   }
 
   /**
    * 隐式函数定义
    */
   implicit def intToString(x: Int) = x.toString
+
+}
+
+object MyConversions {
+
+  implicit def stringWrapper(s: String): IndexedSeq[Char] = s.appended()
+
+  implicit def intToString(x: Int): String = String.valueOf(x)
 
 }
