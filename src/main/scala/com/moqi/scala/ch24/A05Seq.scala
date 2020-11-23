c4e3c3ffcf7b8b3bbf0bc5b9b37904cf06fd7220
moqi <moqimoqidea@gmail.com>
2020-11-23 16:45:17 +0800

    序列型特质 Seq、IndexedSeq 和 LinearSeq

---
+++ src/main/scala/com/moqi/scala/ch24/A05Seq.scala
@@ -0,0 +1,16 @@
+package com.moqi.scala.ch24
+
+/**
+ * 序列型特质 Seq、IndexedSeq 和 LinearSeq
+ *
+ * @author moqi On 11/23/20 16:18
+ */
+object A05Seq {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
