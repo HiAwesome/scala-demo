d6f634a39f4a99f645d960b492514cc47ae84535
moqi <moqimoqidea@gmail.com>
2020-11-23 11:11:18 +0800

    用 for 推导式进行翻译

---
+++ src/main/scala/com/moqi/scala/ch23/A04TranslationOfForExpressions.scala
@@ -0,0 +1,17 @@
+package com.moqi.scala.ch23
+
+/**
+ * 用 for 推导式进行翻译
+ * 每个 for 表达式都可以用三个高阶函数 map、flatMap 和 withFilter 来表示
+ *
+ * @author moqi On 11/23/20 10:54
+ */
+object A04TranslationOfForExpressions {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
