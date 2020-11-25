b1a4f107096e1deec1d1ffb80ec42331cc378472
moqi <moqimoqidea@gmail.com>
2020-11-25 11:51:09 +0800

    抽取公共操作

---
+++ src/main/scala/com/moqi/scala/ch25/A02ExtractCommonOperations.scala
@@ -0,0 +1,17 @@
+package com.moqi.scala.ch25
+
+/**
+ * 抽取公共操作
+ * Scala 用静态的隐式解析规则来解决 map 的类型约束，用虚拟分发来选择与这些约束相对应的最佳动态（运行时）类型
+ *
+ * @author moqi On 11/25/20 10:56
+ */
+object A02ExtractCommonOperations {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
