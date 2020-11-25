93c72032a2772a5ff0a147232108d58e4aff2ece
moqi <moqimoqidea@gmail.com>
2020-11-25 16:32:29 +0800

    提取邮箱地址

---
+++ src/main/scala/com/moqi/scala/ch26/A01ExtractingEmailAddresses.scala
@@ -0,0 +1,16 @@
+package com.moqi.scala.ch26
+
+/**
+ * 提取邮箱地址
+ *
+ * @author moqi On 11/25/20 16:30
+ */
+object A01ExtractingEmailAddresses {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
