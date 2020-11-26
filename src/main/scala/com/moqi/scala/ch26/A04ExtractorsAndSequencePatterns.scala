8da11b01e92ebb343d17aee00b7c231c57a669bc
moqi <moqimoqidea@gmail.com>
2020-11-26 10:21:35 +0800

    提取器和序列模式

---
+++ src/main/scala/com/moqi/scala/ch26/A04ExtractorsAndSequencePatterns.scala
@@ -0,0 +1,16 @@
+package com.moqi.scala.ch26
+
+/**
+ * 提取器和序列模式
+ *
+ * @author moqi On 11/26/20 10:09
+ */
+object A04ExtractorsAndSequencePatterns {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
