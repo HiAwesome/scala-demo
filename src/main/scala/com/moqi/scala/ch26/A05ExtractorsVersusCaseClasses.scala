5bd5c950c692e169c5e4e1e9ba9db014bf6a0247
moqi <moqimoqidea@gmail.com>
2020-11-26 10:29:57 +0800

    提取器和样例类对比

---
+++ src/main/scala/com/moqi/scala/ch26/A05ExtractorsVersusCaseClasses.scala
@@ -0,0 +1,16 @@
+package com.moqi.scala.ch26
+
+/**
+ * 提取器和样例类对比
+ *
+ * @author moqi On 11/26/20 10:21
+ */
+object A05ExtractorsVersusCaseClasses {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
