34112cea43bde42372f1d775c7ec7a948d90fc8b
moqi <moqimoqidea@gmail.com>
2020-11-26 17:48:24 +0800

    运行时链接

---
+++ src/main/scala/com/moqi/scala/ch29/A05RuntimeLinking.scala
@@ -0,0 +1,39 @@
+package com.moqi.scala.ch29
+
+/**
+ * 运行时链接
+ *
+ * @author moqi On 11/26/20 17:40
+ */
+object A05RuntimeLinking {
+
+  def main(args: Array[String]): Unit = {
+
+
+  }
+
+}
+
+/**
+ * 动态选择模块实现的应用
+ */
+object GotApples {
+
+  def main(args: Array[String]): Unit = {
+
+    val db: Database =
+      if (args(0) == "student")
+        StudentDatabase
+      else
+        SimpleDatabase
+
+    object browser extends Browser {
+      override val database: Database = db
+    }
+
+    val apple = SimpleDatabase.foodNamed("Apple").get
+    for (recipe <- browser.recipesUsing(apple)) println(s"recipe = ${recipe}")
+
+  }
+
+}
