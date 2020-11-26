4e49be761b44a60e0a485be252b6fedff3fe328e
moqi <moqimoqidea@gmail.com>
2020-11-26 11:16:59 +0800

    XML 概览

---
+++ src/main/scala/com/moqi/scala/ch28/A02XMLOverview.scala
@@ -0,0 +1,17 @@
+package com.moqi.scala.ch28
+
+/**
+ * XML 概览
+ *
+ * @author moqi On 11/26/20 11:13
+ */
+object A02XMLOverview {
+
+  def main(args: Array[String]): Unit = {
+
+    val x1 = "<pod> Three <peas/> in the </pod>"
+    println(s"x1 = ${x1}")
+
+  }
+
+}
