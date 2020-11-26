b9939b138c9dbd59315b12d6dc1c44b7778299e1
moqi <moqimoqidea@gmail.com>
2020-11-26 19:15:58 +0800

    Scala 中的相等性

---
+++ src/main/scala/com/moqi/scala/ch30/A01EqualityInScala.scala
@@ -0,0 +1,21 @@
+package com.moqi.scala.ch30
+
+/**
+ * Scala 中的相等性
+ *
+ * @author moqi On 11/26/20 19:12
+ */
+object A01EqualityInScala {
+
+  def main(args: Array[String]): Unit = {
+
+  }
+
+  final def ==(that: AnyRef): Boolean =
+    if (null eq this) {
+      null eq that
+    } else {
+      this equals that
+    }
+
+}
