400b17fad94a1dc6c722906a8bf846ba7c95d8ea
moqi <moqimoqidea@gmail.com>
2020-11-26 11:53:42 +0800

    序列化

---
+++ src/main/scala/com/moqi/scala/ch28/A04Serialization.scala
@@ -0,0 +1,72 @@
+package com.moqi.scala.ch28
+
+import scala.xml.Elem
+
+/**
+ * 序列化
+ *
+ * @author moqi On 11/26/20 11:46
+ */
+object A04Serialization {
+
+  def main(args: Array[String]): Unit = {
+
+    func1
+
+    func2
+
+  }
+
+  private def func2: Unit = {
+    val x1 = <a>{{{{brace yourself!}}}}</a>
+    println(s"x1 = ${x1}")
+    println()
+  }
+
+  private def func1: Unit = {
+    val therm = new CCTherm {
+      override val description: String = "hot dog #5"
+      override val yearMade: Int = 1952
+      override val dateObtained: String = "March 14, 2006"
+      override val bookPrice: Int = 2199
+      override val purchasePrice: Int = 500
+      override val condition: Int = 9
+    }
+
+    println(s"therm.toXML = ${therm.toXML}")
+    println()
+  }
+}
+
+abstract class CCTherm {
+  val description: String
+  val yearMade: Int
+  val dateObtained: String
+  val bookPrice: Int
+  val purchasePrice: Int
+  val condition: Int
+
+  override def toString: String = description
+
+  def toXML: Elem =
+    <cctherm>
+      <description>
+        {description}
+      </description>
+      <yearMade>
+        {yearMade}
+      </yearMade>
+      <dateObtained>
+        {dateObtained}
+      </dateObtained>
+      <bookPrice>
+        {bookPrice}
+      </bookPrice>
+      <purchasePrice>
+        {purchasePrice}
+      </purchasePrice>
+      <condition>
+        {condition}
+      </condition>
+    </cctherm>
+}
