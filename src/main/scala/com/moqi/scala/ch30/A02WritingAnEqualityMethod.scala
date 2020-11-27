e75986a1edc710747e6af78487c630e3d5c9ebdc
moqi <moqimoqidea@gmail.com>
2020-11-27 11:13:57 +0800

    编写相等性方法 v4

--- src/main/scala/com/moqi/scala/ch30/A02WritingAnEqualityMethod.scala
+++ src/main/scala/com/moqi/scala/ch30/A02WritingAnEqualityMethod.scala
@@ -15,6 +15,18 @@ object A02WritingAnEqualityMethod {
 
     func2
 
+    func3
+
+  }
+
+  private def func3: Unit = {
+    val p = new Point2(1, 2)
+    val set = mutable.HashSet(p)
+    println(s"set contains p = ${set contains p}")
+    p.x += 1
+    println(s"set contains p = ${set contains p}")
+    println(s"set.iterator contains p = ${set.iterator contains p}")
+    println()
   }
 
   private def func2: Unit = {
@@ -58,6 +70,20 @@ class Point(val x: Int, val y: Int) {
    * ## 是计算基本类型、引用类型和 null 的 hashCode 的简写方式
    * 另可参考：https://stackoverflow.com/a/9068566
    */
+  override def hashCode(): Int = (x, y).##
+
+}
+
+/**
+ * 用可变字段定义 equals
+ */
+class Point2(var x: Int, var y: Int) {
+
+  override def equals(other: Any): Boolean = other match {
+    case that: Point => this.x == that.x && this.y == that.y
+    case _ => false
+  }
+
   override def hashCode(): Int = (x, y).##
 
 }
