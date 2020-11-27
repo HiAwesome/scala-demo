e58c1f96969a5bb99a5edd425cf68e52226a3b4f
moqi <moqimoqidea@gmail.com>
2020-11-27 17:32:32 +0800

    同时编译 Scala 和 Java

---
+++ src/main/scala/com/moqi/scala/ch31/A04CompilingScalaAndJavaTogether.scala
@@ -0,0 +1,16 @@
+package com.moqi.scala.ch31
+
+/**
+ * 同时编译 Scala 和 Java
+ *
+ * @author moqi On 11/27/20 16:25
+ */
+object A04CompilingScalaAndJavaTogether {
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
