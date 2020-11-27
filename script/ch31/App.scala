c3151b4e7faff79d89a32dd0feea0a586aa9534e
moqi <moqimoqidea@gmail.com>
2020-11-27 15:36:34 +0800

    结合 Scala 和 Java

---
+++ script/ch31/App.scala
@@ -0,0 +1,16 @@
+/**
+ * ~/Code/scala-demo/script/ch31(main ✗) javap App
+ * Compiled from "App.scala"
+ * public final class App {
+ *   public static void main(java.lang.String[]);
+ * }
+ */
+object App {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Hello World")
+
+  }
+
+}
---
+++ src/main/scala/com/moqi/scala/ch31/A01UsingScalaFromJava.scala
@@ -0,0 +1,16 @@
+package com.moqi.scala.ch31
+
+/**
+ * 结合 Scala 和 Java
+ *
+ * @author moqi On 11/27/20 15:31
+ */
+object A01UsingScalaFromJava {
+
+  def main(args: Array[String]): Unit = {
+
+    println("Hello World")
+
+  }
+
+}
