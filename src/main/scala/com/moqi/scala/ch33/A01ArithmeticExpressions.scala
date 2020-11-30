f4c79eb075f38219fde2dda78b83cd8cfe753af7
moqi <moqimoqidea@gmail.com>
2020-11-30 16:16:01 +0800

    算术表达式

---
+++ src/main/scala/com/moqi/scala/ch33/A01ArithmeticExpressions.scala
@@ -0,0 +1,29 @@
+package com.moqi.scala.ch33
+
+import scala.util.parsing.combinator._
+
+/**
+ * 算术表达式
+ *
+ * @author moqi On 11/30/20 16:04
+ */
+object A01ArithmeticExpressions {
+  def main(args: Array[String]): Unit = {
+
+
+  }
+
+}
+
+/**
+ * 算术表达式解析器
+ */
+class Arith extends JavaTokenParsers {
+
+  def expr: Parser[Any] = term - rep("+" ~ term | "-" ~ term)
+
+  def term: Parser[Any] = factor ~ rep("*" ~ factor | "/" ~ factor)
+
+  def factor: Parser[Any] = floatingPointNumber | "(" ~ expr ~ ")"
+
+}
