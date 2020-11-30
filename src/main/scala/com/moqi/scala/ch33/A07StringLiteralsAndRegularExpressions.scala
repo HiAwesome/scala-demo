3d8b7c50d55012d71cbad45134172c95336b685d
moqi <moqimoqidea@gmail.com>
2020-11-30 17:46:25 +0800

    字符串字面量和正则表达式

---
+++ src/main/scala/com/moqi/scala/ch33/A07StringLiteralsAndRegularExpressions.scala
@@ -0,0 +1,20 @@
+package com.moqi.scala.ch33
+
+import scala.util.parsing.combinator.RegexParsers
+
+/**
+ * 字符串字面量和正则表达式
+ *
+ * @author moqi On 11/30/20 17:03
+ */
+object A07StringLiteralsAndRegularExpressions extends RegexParsers {
+
+  override val whiteSpace = "".r
+
+  def main(args: Array[String]): Unit = {
+
+
+
+  }
+
+}
